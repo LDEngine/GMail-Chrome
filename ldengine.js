8ca425bdd0b05add0c429ae7efa1a30904905b32
Curtis Lacy <curtis.lacy@ldengine.com>
2013-03-13 20:04:54 -0400

    Moved extension image files over to the server, and started trying to get a messaging-based mechanism for CSS injection to work.

--- ldengine.js
+++ ldengine.js
@@ -1,6 +1,36 @@
 // Bootstrap
 $(function() {
 
+
+console.log( 'A' );
+	chrome.extension.onConnect.addListener(function(port) {
+		console.log( 'I' );
+  console.assert(port.name == "knockknock");
+  console.log( 'E' );
+  port.onMessage.addListener(function(msg) {
+  	console.log( 'H' );
+  	console.log( msg );
+    if (msg.joke == "Knock knock")
+      port.postMessage({question: "Who's there?"});
+    else if (msg.answer == "Madame")
+      port.postMessage({question: "Madame who?"});
+    else if (msg.answer == "Madame... Bovary")
+      port.postMessage({question: "I don't get it."});
+  });
+});
+	console.log( 'B' );
+
+
+	/*chrome.extension.onMessage.addListener( 
+		function( request, sender, sendResponse ) {
+			alert( 'Got request:' + request );
+			sendResponse( JSON.stringify( {
+				success: true
+			} ));
+		}
+		//chrome.tabs.insertCSS( { file: "http://127.0.0.1:3000/GMail-Chrome/ldengine.css" } );
+	);*/
+
 	chrome.storage.local.get('engine_api_url', function(items) {
 
 		// If there's nothing in there, default to the default production version.
