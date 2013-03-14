29fb5f9e6201126ccdd5ccae73965377b97b1361
Curtis Lacy <curtis.lacy@ldengine.com>
2013-03-13 22:47:44 -0400

    Moved most of the js files to the server, along with the css files.  Adjusted ldengine.js so that it loads CSS and JS from the server.

--- ldengine.js
+++ ldengine.js
@@ -1,53 +1,51 @@
 // Bootstrap
 $(function() {
 
-
-console.log( 'A' );
-	chrome.extension.onConnect.addListener(function(port) {
-		console.log( 'I' );
-  console.assert(port.name == "knockknock");
-  console.log( 'E' );
-  port.onMessage.addListener(function(msg) {
-  	console.log( 'H' );
-  	console.log( msg );
-    if (msg.joke == "Knock knock")
-      port.postMessage({question: "Who's there?"});
-    else if (msg.answer == "Madame")
-      port.postMessage({question: "Madame who?"});
-    else if (msg.answer == "Madame... Bovary")
-      port.postMessage({question: "I don't get it."});
-  });
-});
-	console.log( 'B' );
-
-
-	/*chrome.extension.onMessage.addListener( 
-		function( request, sender, sendResponse ) {
-			alert( 'Got request:' + request );
-			sendResponse( JSON.stringify( {
-				success: true
-			} ));
-		}
-		//chrome.tabs.insertCSS( { file: "http://127.0.0.1:3000/GMail-Chrome/ldengine.css" } );
-	);*/
+	console.log( 'Starting local bootstrap.' );
 
 	chrome.storage.local.get('engine_api_url', function(items) {
 
 		// If there's nothing in there, default to the default production version.
 		API_URL = items.engine_api_url || "https://apps.engine.co";
-		console.log( 'Starting Bootstrap Function' );
-
+		
 		// If there's no protocol specified, use https by default.
 		if( API_URL.indexOf( "http" ) < 0 )
 			API_URL = "https://" + API_URL;
 
-		$.get( API_URL + '/GMail-Chrome/ldengine.js', null, 
-			function( data, textStatus, jqXHR ) {
-				bootstrap();
-			} 
-		).fail( function() {
-			alert( 'Could not load Engine extension code from ' + API_URL );
-		});
+		$.get( API_URL + '/GMail-Chrome/Injects.json', null, 
+			function( injects, textStatus, jqXHR ) {
+
+				injects.css.forEach( function( item ) {
+
+					var link = document.createElement("link");
+					link.href = API_URL + '/GMail-Chrome/' + item ;
+					link.type = "text/css";
+					link.rel = "stylesheet";
+					document.getElementsByTagName("head")[0].appendChild(link);
+
+				});
+
+				async.forEachSeries( injects.js,
+					function( item, done ) {
+						$.get( API_URL + '/GMail-Chrome/' + item, null, 
+							function( data, textStatus, jqXHR ) {
+								console.log( 'Loaded ' + item );
+								done();
+							} 
+						).fail( function() {
+							alert( 'Could not load Engine extension code from ' + API_URL + '/GMail-Chrome/' + item );
+							done();
+						});
+					},
+					function( error ) {
+						bootstrap();
+					}
+				);
+		
+			}).fail( function() {
+				alert( 'Could not load CSS list from ' + API_URL );
+			});
+
 
 	});
 });
