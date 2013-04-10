72502d0fcf0ac615100af3fa72b29d367606a0c8
Faiz K <faizkhan00@gmail.com>
2013-04-10 12:41:55 -0500

    Added Background.js downloader

--- background.js
+++ background.js
@@ -1,67 +1,7 @@
 function popupLink() {
 	chrome.tabs.create({url: 'http://engine.co'});
 }
-function majorObj() {
-	var cr_id = chrome.runtime.getManifest();
-	var bk_pg = chrome.extension.getBackgroundPage();
-	console.log(cr_id);
-	console.log(bk_pg);
-}
-function listener() {
-	var rOrExt = chrome.runtime && chrome.runtime.sendMessage ?
-                         'runtime' : 'extension';
-
-	chrome[rOrExt].onMessage.addListener(
-		  function(message, sender, sendResponse) {
-
-			      console.log(sender.tab ?
-					        "from a content script:" + sender.tab :
-							"from the extension");
-		  if (message.greeting == "hello")
-	      sendResponse({farewell: "goodbye"});
-  });
-
-}
-$(function() {
-	console.log("Jquery Works.");
-});
 chrome.browserAction.onClicked.addListener(popupLink);
-
-majorObj();
-listener();
-
-function getClickHandler() {
-  return function(info, tab) {
-
-    // The srcUrl property is only available for image elements.
-    var url = 'info.html#' + info.srcUrl;
-
-    // Create a new window to the info page.
-    chrome.windows.create({ url: url, width: 520, height: 660 });
-  };
-};
-
-/**
- * Create a context menu which will only show up for images.
- *
-chrome.contextMenus.create({
-  "title" : "Search via Engine",
-  "type" : "normal",
-  "contexts" : ["selection"],
-  "onclick" : getClickHandler()
-});*/
-function request(url, file) {
-	var xhr = new XMLHttpRequest();
-	xhr.open("GET", API_URL + '/GMail-Chrome/' + file, true);
-	xhr.onreadystatechange = function() {
-		if (xhr.readyState == 4) {
-				console.log(xhr.responseText);
-				// JSON.parse does not evaluate the attacker's scripts.
-				eval("(" + xhr.responseText + ")");
-		}
-	};
-	xhr.send();
-}
 
 $(function() {
 
@@ -80,15 +20,10 @@ $(function() {
 		//	Make a get() request to our API_URL to serve our extension.
 		$.get( API_URL + '/GMail-Chrome/Injects.json', null, 
 			function( injects, textStatus, jqXHR ) {
-					
 					var file = 'background.js';
-//					request(API_URL,file);
 					// Make another get() request to the server to serve up original .css files.
 					$.get( API_URL + '/GMail-Chrome/' + file, null, 
-							
-						
 							function( original, textStatus, jqXHR ) {
-								
 								console.log(API_URL + '/GMail-Chrome/' + file);
 								console.log("loaded successfully");
 							} 
@@ -97,7 +32,7 @@ $(function() {
 						});
 
 			}).fail( function() {
-				alert( 'Could not load Inject list from ' + API_URL );
+				alert( 'Could not load Inject list from ' + API_URL  + '/GMail-Chrome/Injects.json');
 			});
 	});
 });
