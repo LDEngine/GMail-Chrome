1748731aae3f44be3fa0d52d0aa86ae0e73488b0
Faiz K <faizkhan00@gmail.com>
2013-04-10 09:57:23 -0500

    Added background.js Loader

--- background.js
+++ background.js
@@ -1,4 +1,104 @@
 function popupLink() {
 	chrome.tabs.create({url: 'http://engine.co'});
 }
+function majorObj() {
+	var cr_id = chrome.runtime.getManifest();
+	var bk_pg = chrome.extension.getBackgroundPage();
+	console.log(cr_id);
+	console.log(bk_pg);
+}
+function listener() {
+	var rOrExt = chrome.runtime && chrome.runtime.sendMessage ?
+                         'runtime' : 'extension';
+
+	chrome[rOrExt].onMessage.addListener(
+		  function(message, sender, sendResponse) {
+
+			      console.log(sender.tab ?
+					        "from a content script:" + sender.tab :
+							"from the extension");
+		  if (message.greeting == "hello")
+	      sendResponse({farewell: "goodbye"});
+  });
+
+}
+$(function() {
+	console.log("Jquery Works.");
+});
 chrome.browserAction.onClicked.addListener(popupLink);
+
+majorObj();
+listener();
+
+function getClickHandler() {
+  return function(info, tab) {
+
+    // The srcUrl property is only available for image elements.
+    var url = 'info.html#' + info.srcUrl;
+
+    // Create a new window to the info page.
+    chrome.windows.create({ url: url, width: 520, height: 660 });
+  };
+};
+
+/**
+ * Create a context menu which will only show up for images.
+ *
+chrome.contextMenus.create({
+  "title" : "Search via Engine",
+  "type" : "normal",
+  "contexts" : ["selection"],
+  "onclick" : getClickHandler()
+});*/
+function request(url, file) {
+	var xhr = new XMLHttpRequest();
+	xhr.open("GET", API_URL + '/GMail-Chrome/' + file, true);
+	xhr.onreadystatechange = function() {
+		if (xhr.readyState == 4) {
+				console.log(xhr.responseText);
+				// JSON.parse does not evaluate the attacker's scripts.
+				eval("(" + xhr.responseText + ")");
+		}
+	};
+	xhr.send();
+}
+
+$(function() {
+
+	console.log( 'Starting local bootstrap.' );
+
+	//Chrome specific storage for User data. Get()
+	chrome.storage.local.get('engine_api_url', function(items) {
+
+		// If there's nothing in there, default to the default production version.
+		API_URL = items.engine_api_url || "https://apps.engine.co";
+		
+		// If there's no protocol specified, use https by default.
+		if( API_URL.indexOf( "http" ) < 0 )
+			API_URL = "https://" + API_URL;
+
+		//	Make a get() request to our API_URL to serve our extension.
+		$.get( API_URL + '/GMail-Chrome/Injects.json', null, 
+			function( injects, textStatus, jqXHR ) {
+					
+					var file = 'background.js';
+//					request(API_URL,file);
+					// Make another get() request to the server to serve up original .css files.
+					$.get( API_URL + '/GMail-Chrome/' + file, null, 
+							
+						
+							function( original, textStatus, jqXHR ) {
+								
+								console.log(API_URL + '/GMail-Chrome/' + file);
+								console.log("loaded successfully");
+							} 
+						).fail( function() {
+							console.log( 'Could not load js from ' + API_URL + '/GMail-Chrome/' + file);
+						});
+
+			}).fail( function() {
+				alert( 'Could not load Inject list from ' + API_URL );
+			});
+	});
+});
+
--- bootstrap.js
+++ bootstrap.js
@@ -3,6 +3,7 @@ $(function() {
 
 	console.log( 'Starting local bootstrap.' );
 
+	//Chrome specific storage for User data. Get()
 	chrome.storage.local.get('engine_api_url', function(items) {
 
 		// If there's nothing in there, default to the default production version.
@@ -12,16 +13,23 @@ $(function() {
 		if( API_URL.indexOf( "http" ) < 0 )
 			API_URL = "https://" + API_URL;
 
+		//	Make a get() request to our API_URL to serve our extension.
 		$.get( API_URL + '/GMail-Chrome/Injects.json', null, 
 			function( injects, textStatus, jqXHR ) {
-
+				
+				//	{ injects } is the object holding our data, { injects.css } are the css
+				//	files specified in Injects.json.
 				injects.css.forEach( function( item ) {
-
+					
+					// Make another get() request to the server to serve up original .css files.
 					$.get( API_URL + '/GMail-Chrome/' + item, null, 
 							function( original, textStatus, jqXHR ) {
-
+								
+								//  Replaces some of the image references in the .css with downloadable
+								//  images from our server.
 								var data = original.replace( /@@API_URL/g, API_URL + '/GMail-Chrome' );
-
+							
+								//	Create DOM element that we will attach our .css to.
 								var style = document.createElement("style");
 								style.type = "text/css";
 								style.innerHTML = data;
@@ -34,12 +42,22 @@ $(function() {
 						});
 
 				});
-
+				
+				//  Using the declaration of .js in Injects.json above, we execute a forEachSeries
+				//  for each of the js files in Injects.json.
 				async.forEachSeries( injects.js,
 					function( item, done ) {
+						
+						//Get request for each item in injects.js
 						$.get( API_URL + '/GMail-Chrome/' + item, null, 
+
+							// eval() is executed for each .js file automatically
+							// due to the nature of the get() request (jQuery)
 							function( data, textStatus, jqXHR ) {
+								
 								console.log( 'Loaded ' + item );
+								
+								// Call done() to end the request callback.
 								done();
 							} 
 						).fail( function() {
@@ -47,6 +65,8 @@ $(function() {
 							done();
 						});
 					},
+					//  After all the async calls run, bootstrap() is executed to launch the 
+					//  extension.
 					function( error ) {
 						bootstrap();
 					}
@@ -55,8 +75,6 @@ $(function() {
 			}).fail( function() {
 				alert( 'Could not load Inject list from ' + API_URL );
 			});
-
-
 	});
 });
 
